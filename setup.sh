a37c01d55246144a403b9c60c472cfdcc881a8e7
danielh13210 <danielh13210@gmail.com>
2025-08-06 15:45:22 +0800

    Add Dockerfile

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 python3 -m venv penguindrop-controller
 source penguindrop-controller/bin/activate
 pip3 install flask
+pushd penguindrop-acceptor
+docker build -t penguindrop-acceptor .
+popd
