24c397061db478290f24762ab914a6216d33910f
danielh13210 <danielh13210@gmail.com>
2025-10-04 08:04:01 +0800

    Use a notification instead of zenity
    zenity is interrupting, notifications are noninterrupting

--- confirmation.sh
+++ confirmation.sh
@@ -5,16 +5,19 @@ key="${2}"
 name="${3}"
 KILLED=false
 
-zenity --question --title="File Sharing" --text="Would you like to recieve the file ${filename} from ${name}?" &
+mktemp 
+notify-send -i email -A "accept"=Accept -A "decline"=Decline -e "PenguinDrop: Accept file?" "Accept file ${filename} from ${name}?" >  /tmp/accepted &
 ZPID=$!
 trap "kill $ZPID;KILLED=true" INT TERM
 wait $ZPID
-EXITCODE=$?
 if [ "$KILLED" = false ]; then
-    if [ $EXITCODE -eq 0 ]; then
+    if [ "$(</tmp/accepted)" == "accept" ]; then
         result=true
     else
         result=false
     fi
     curl -H "Content-Type: application/json" -d '{"key":"'"${key}"'","accept":'"${result}"'}' "http://127.0.0.1:6707/confirm"
+fi
+if [ -f /tmp/accepted ]; then
+  rm /tmp/accepted
 fi
