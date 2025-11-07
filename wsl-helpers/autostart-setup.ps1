075a7bcec24a1ce4aa20d71e9b39f6c1de9493aa
daniel <danielh13210@gmail.com>
2025-11-07 15:33:19 +0800

    Make the powershell window actually vanish
    Start-Process detaches from the Windows Terminal, which allows -WindowStyle Hidden to work

--- wsl-helpers/autostart-setup.ps1
+++ wsl-helpers/autostart-setup.ps1
@@ -3,7 +3,7 @@ $WshShell = New-Object -ComObject WScript.Shell
 # Define shortcut path and target
 $shortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\pdserver.lnk"
 $targetPath = "powershell.exe"
-$arguments = "-WindowStyle Hidden -Command wsl.exe bash $($args[0])"
+$arguments = "-Command Start-Process -FilePath wsl.exe -WindowStyle Hidden -ArgumentList 'bash $($args[0])'"
 
 # Create the shortcut
 $shortcut = $WshShell.CreateShortcut($shortcutPath)
