93a151b609645b821e5093ab01b1b89764bd5403
daniel <danielh13210@gmail.com>
2025-11-07 14:39:31 +0800

    Exit early if venv creation failed

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE[0]}")"
-python3 -m venv penguindrop-controller
+python3 -m venv penguindrop-controller || exit 1
 source penguindrop-controller/bin/activate
 pip3 install flask
 pushd penguindrop-acceptor
